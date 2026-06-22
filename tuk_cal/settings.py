48242e1159b92c660b9c92ca3ccb5b18201e269f
dev-kizan <kizanfabian99@gmail.com>
2026-06-23 02:31:24 +0530

    assets: add whitenoise to serve production css

--- tuk_cal/settings.py
+++ tuk_cal/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -144,8 +145,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'tuk_cal/static/'
-STATICFILES_DIRS = [BASE_DIR / 'tuk_cal/static']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / '/static']
 
-MEDIA_URL = 'tuk_cal/media/'
-MEDIA_ROOT = BASE_DIR / 'tuk_cal/media'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
